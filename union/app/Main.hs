180170c2eced67144dade4f77d9073c5d08e1a3e
Avilladat <avilladat@eafit.edu.co>
2021-09-14 01:38:26 -0500

    Function union

--- union/app/Main.hs
+++ union/app/Main.hs
@@ -10,14 +10,14 @@ import Language.Mira.FA.Types
 
 ----------------------------------------------
 -- Automaton accepting strings ending in 01
-fa1 :: FA Natural
+fa1 :: FA Int
 fa1 = MkFA (Set.fromList [1,2,3])
            (Set.fromList [Move 1 '1' 1, Move 1 '0' 2, Move 2 '1' 3, Move 2 '0' 2, Move 3 '1' 1, Move 3 '0' 2])
            (1)
            (Set.fromList [3]) 
 
 -- Automaton accepting strings begining in 10
-fa2 :: FA Natural
+fa2 :: FA Int
 fa2 = MkFA (Set.fromList [1,2,3])
            (Set.fromList[Move 1 '1' 2, Move 2 '0' 3, Move 3 '1' 3, Move 3 '0' 3])
            (1)
--- union/src/Union.hs
+++ union/src/Union.hs
@@ -5,5 +5,21 @@ import Data.Set ( Set )
 import qualified Data.Set as Set
 import Language.Mira.FA.Types
 
-union :: FA Natural -> FA Natural -> Int
-union fa1 fa2 = 42
+union :: FA Int -> FA Int -> FA Int
+union ((MkFA fa1States fa1Moves fa1StartState fa1FinalStates))
+      ((MkFA fa2States fa2Moves fa2StartState fa2FinalStates)) 
+
+    = (MkFA (Set.union (Set.union states1 states2) newstates)
+            (Set.union (Set.union moves1 moves2) newmoves)
+            (0)
+            (Set.fromList [n+m+1]))
+
+    where
+    n = Set.size fa1States
+    m = Set.size fa2States
+    states1 = Set.fromList [0..n]
+    states2 = Set.fromList [n+1..n+m]
+    newstates = Set.fromList [0,(n+m+1)]
+    newmoves = Set.fromList [Emove 0 1, Emove 0 (n+1), Emove n (n+m+1), Emove (n+m) (n+m+1)] 
+    moves1 = fa1Moves
+    moves2 = Set.fromList [Emove 0 0]
