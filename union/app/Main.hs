06158387a60620180046a7eea47df2bbc4b53477
Avilladat <avilladat@eafit.edu.co>
2021-09-14 01:20:48 -0500

    Imports and functions

--- union/app/Main.hs
+++ union/app/Main.hs
@@ -1,28 +1,29 @@
 module Main where
 
--- Para crear los autóamatas mecesito estos datos
-import Data.Set ( Set )
-import qualified Data.Set as Set
+----------------------------------------------
 
-import Composition
+import Union
+import Numeric.Natural (Natural)
+import Data.Set ( Set )                 
+import qualified Data.Set as Set              
+import Language.Mira.FA.Types 
 
-import Language.Mira.FA.Types
+----------------------------------------------
+-- Automaton accepting strings ending in 01
+fa1 :: FA Natural
+fa1 = MkFA (Set.fromList [1,2,3])
+           (Set.fromList [Move 1 '1' 1, Move 1 '0' 2, Move 2 '1' 3, Move 2 '0' 2, Move 3 '1' 1, Move 3 '0' 2])
+           (1)
+           (Set.fromList [3]) 
 
-fa1 :: FA Int
-fa1 = MkFA 
-  (Set.fromList [1,2,3])
-  (Set.fromList [Move 1 'a' 2, Emove 3 3])
-  2
-  (Set.fromList [2])
-
-fa2 :: FA Int
-fa2 = MkFA 
-  (Set.fromList [1,2,3])
-  (Set.fromList [Move 1 'a' 2, Emove 3 3])
-  2
-  (Set.fromList [2])
+-- Automaton accepting strings begining in 10
+fa2 :: FA Natural
+fa2 = MkFA (Set.fromList [1,2,3])
+           (Set.fromList[Move 1 '1' 2, Move 2 '0' 3, Move 3 '1' 3, Move 3 '0' 3])
+           (1)
+           (Set.fromList [3]) 
+----------------------------------------------
 
 main :: IO ()
--- main = someFunc
 main = print $ union fa1 fa2
 
--- union/src/Composition.hs
+++ union/src/Composition.hs
@@ -1,12 +1,9 @@
-module Composition
-    ( union
-    ) where
+module Union (union) where
 
--- someFunc :: IO ()
--- someFunc = putStrLn "someFunc"
-
+import Numeric.Natural ( Natural )
+import Data.Set ( Set )                 
+import qualified Data.Set as Set
 import Language.Mira.FA.Types
 
-union :: FA Int -> FA Int -> Int
+union :: FA Natural -> FA Natural -> Int
 union fa1 fa2 = 42
-
