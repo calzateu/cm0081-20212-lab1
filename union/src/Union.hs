7dda5e9ddda03d028f44558ffd5e52451ad4ea28
Avilladat <avilladat@eafit.edu.co>
2021-09-14 03:54:20 -0500

    Auxiliar functions

--- union/src/Union.hs
+++ union/src/Union.hs
@@ -5,7 +5,15 @@ import Data.Set ( Set )
 import qualified Data.Set as Set
 import Language.Mira.FA.Types
 
-union :: FA Int -> FA Int -> FA Int
+renumberAux :: Move Natural -> Natural -> Move Natural
+renumberAux (Move a c b) n = Move (a+n) c (b+n) 
+renumberAux (Emove a b) n = Emove (a+n) (b+n) 
+
+renumberSet :: [Move Natural] -> Natural -> [Move Natural] 
+renumberSet [] n = []
+renumberSet (x:xs) n = ([renumberAux x n]) ++ (renumberSet xs n) 
+
+union :: FA Natural -> FA Natural -> FA Natural
 union ((MkFA fa1States fa1Moves fa1StartState fa1FinalStates))
       ((MkFA fa2States fa2Moves fa2StartState fa2FinalStates)) 
 
@@ -15,11 +23,11 @@ union ((MkFA fa1States fa1Moves fa1StartState fa1FinalStates))
             (Set.fromList [n+m+1]))
 
     where
-    n = Set.size fa1States
-    m = Set.size fa2States
+    n = fromIntegral (Set.size fa1States)
+    m = fromIntegral (Set.size fa2States)
     states1 = Set.fromList [0..n]
     states2 = Set.fromList [n+1..n+m]
     newstates = Set.fromList [0,(n+m+1)]
     newmoves = Set.fromList [Emove 0 1, Emove 0 (n+1), Emove n (n+m+1), Emove (n+m) (n+m+1)] 
     moves1 = fa1Moves
-    moves2 = Set.fromList [Emove 0 0]
+    moves2 = Set.fromList (renumberSet (Set.toList fa2Moves) n)
